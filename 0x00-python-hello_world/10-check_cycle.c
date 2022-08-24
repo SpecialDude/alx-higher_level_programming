35cdf8af42eb55dd9fcfd2036d1bf84604a9cc91
SpecialDude <warithadetayo.awa@gmail.com>
2022-08-24 01:27:20 +0100

    Hello Python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * check_cycle - Check if a cycle exists within a linked list
+ *
+ * @list: The linked list
+ *
+ * Return: 1 if cycle exists 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+}
