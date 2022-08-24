f7f8b07dec4652915d4b5406716e4b17d8e4b148
SpecialDude <warithadetayo.awa@gmail.com>
2022-08-24 20:46:19 +0100

    Few Changes

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,4 +8,18 @@
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *fast_p, *slow_p;
+
+	fast_p = list;
+	slow_p = list;
+
+	while (slow_p && fast_p && fast_p->next)
+	{
+		slow_p = slow_p->next;
+		fast_p = fast_p->next->next;
+
+		if (slow_p == fast_p)
+			return (1);
+	}
+	return (0);
 }
