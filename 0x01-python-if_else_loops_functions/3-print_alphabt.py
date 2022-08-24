21fd39c22fd91d2d8c89b0f6fb8fa67a24bcc64c
SpecialDude <warithadetayo.awa@gmail.com>
2022-08-24 13:05:24 +0100

    Updated Exercise 0: Hello Python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print('abcdfghijklmnoprstuvwxyz', end='')
+for i in range(ord('a'), ord('z') + 1):
+    if not (chr(i) ==  'e' or chr(i) == 'q'):
+        print("{i:c}".format(i=i), end="")
