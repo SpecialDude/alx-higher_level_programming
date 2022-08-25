ea5f8f8155337f678a21f510cb75c3d7e59039b2
SpecialDude <warithadetayo.awa@gmail.com>
2022-08-25 14:08:19 +0100

    Task 2: Imports

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-import calculator_1 as calc
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     a = 10
     b = 5
 
-    ad = calc.add(a, b)
-    su = calc.sub(a, b)
-    mu = calc.mul(a, b)
-    di = calc.div(a, b)
+    ad = add(a, b)
+    su = sub(a, b)
+    mu = mul(a, b)
+    di = div(a, b)
 
     print("{a} + {b} = {ad}\n{a} - {b} = {su}".format(a=a, b=b, ad=ad, su=su))
     print("{a} * {b} = {mu}\n{a} / {b} = {di}".format(a=a, b=b, mu=mu, di=di))
