26a0011c8a076a296ee1c99205caf6cc4e59defd
SpecialDude <warithadetayo.awa@gmail.com>
2022-08-25 06:52:50 -0700

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+import hidden_4
 
 if __name__ == "__main__":
-    pass
+    print(
+        *list(filter(lambda x: not x.startswith('__'), dir(hidden_4))),
+        sep="\n"
+    )
