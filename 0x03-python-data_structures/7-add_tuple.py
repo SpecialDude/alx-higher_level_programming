75a517ede3643e8417bdd822f7de84cdcd87f975
SpecialDude <warithadetayo.awa@gmail.com>
2022-08-29 00:13:03 +0100

    Data Structure Lists and Tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -13,12 +13,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
     num1 = 0
     num2 = 0
 
-    if len(tuple_a) > 1:
+    if len(tuple_a) > 0:
         num1 += tuple_a[0]
-        num2 += tuple_a[1]
+        num2 += 0 if len(tuple_a) == 1 else tuple_a[1]
 
-    if len(tuple_b) > 1:
+    if len(tuple_b) > 0:
         num1 += tuple_b[0]
-        num2 += tuple_b[1]
+        num2 += 0 if len(tuple_b) == 1 else tuple_b[1]
 
     return (num1, num2)
