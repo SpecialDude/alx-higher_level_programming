3bda42a12e956000eec4e6fb8ae2a1188814f21b
SpecialDude <warithadetayo.awa@gmail.com>
2022-08-29 00:22:26 +0100

    Data Structure Lists and Tuples

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """Find max element in a list
+
+    Args:
+        my_list: List of integers
+
+    Returns:
+        max element
+    """
+
+    if len(my_list) == 0:
+        return None
+
+    max_num = my_list[0]
+
+    for i in range(1, len(my_list)):
+        if my_list[i] > max_num:
+            max_num = my_list[i]
+
+    return max_num
