6e90380309bbf3d786eff380bb70bccfdc1bc11b
SpecialDude <warithadetayo.awa@gmail.com>
2022-08-31 17:56:57 +0100

    More Data Structures: Task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """Common Elements in two sets
+
+    Args:
+        set_1: First Set
+        set_2: Second Set
+
+    Returns:
+        Set of common elements
+    """
+
+    return set_1 & set_2
