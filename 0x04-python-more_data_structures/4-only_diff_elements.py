baa9c138a0aeab7b4e5b4ac655f1042cfe1b3e94
SpecialDude <warithadetayo.awa@gmail.com>
2022-08-31 18:01:25 +0100

    More Data Structures: Task 4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    """Different Elements in two sets
+
+    Args:
+        set_1: First Set
+        set_2: Second Set
+
+    Returns:
+        Set of different elements
+    """
+
+    return set_1 ^ set_2
