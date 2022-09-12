0a9ba06196964d95ff2c3f9c2cc247dafb8c9bbd
Warith Adetayo <warithadetayo.awa@gmail.com>
2022-09-12 19:56:36 +0100

    Update 0-safe_print_list.py
    Fixed Code style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,14 +3,16 @@
 def safe_print_list(my_list=[], x=0):
     """Safely print any list
 
-        Args:
-            my_list: list
-            x: number of elements to print
+    Args:
+        my_list: list
+        x: number of elements to print
 
-        Returns:
-            int
+    Returns:
+        int
     """
+
     c = 0
+
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end = "")
