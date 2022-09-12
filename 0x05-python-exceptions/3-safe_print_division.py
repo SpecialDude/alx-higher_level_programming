7d1e7899a7050691bb7da10f2cbec8db8def0eab
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-12 21:30:22 +0100

    Task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """Safe division
+
+    Args:
+        a: num
+        b: num
+
+    Returns:
+        int
+    """
+
+    result = None
+
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {:d}".format(result))
+
+    return result
