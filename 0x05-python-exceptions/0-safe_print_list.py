8e2e1319f8b45a9cdf1672a006a5cb663cb207f6
Warith Adetayo <warithadetayo.awa@gmail.com>
2022-09-12 19:54:53 +0100

    Update 0-safe_print_list.py
    Redo: Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,8 +15,9 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end = "")
             c += 1
-        print()
     except:
         pass
+
+    print()
 
     return (c)
