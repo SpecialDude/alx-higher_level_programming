b08bbddf0e62bbfc30530d40ff68b7e1526a63ab
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-12 21:50:13 +0100

    Task 6

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """Raise Exception
+
+    Args:
+        message: str
+
+    Returns:
+        None
+    """
+    raise NameError(message)
