5d8006f208e8c280944af52fff2a2135f1d1ea96
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-13 22:34:24 +0100

    Classes: Task 2

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+I swear y'all are bunch of crazy people,
+Yeah I said it (Crazy people)
+
+"""
+
+
+class Square:
+    """Square class with one member variable."""
+    def __init__(self, size=0):
+        """Initializer
+
+        Args:
+            size: size - int
+
+        Returns:
+            None
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Returns the Area of square"""
+
+        return (self.__size ** 2)
