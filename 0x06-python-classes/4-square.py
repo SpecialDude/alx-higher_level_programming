f57fee19f066a63ce0cf6ba4f7eb4be20b247ff8
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-13 22:43:55 +0100

    Classes: Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+"""
+I swear y'all are bunch of crazy people,
+Yeah I said it (Crazy people)
+
+"""
+
+
+class Square:
+    """Square class with one member variable."""
+    def __init__(self, size=0):
+        """Initializer
+
+        Args:
+            size: size - int
+
+        Returns:
+            None
+        """
+
+        self.__validator(size)
+
+        self.__size = size
+
+    def area(self):
+        """Returns the Area of square"""
+
+        return (self.__size ** 2)
+
+    def __validator(self, size):
+        """Validator - Validates size
+
+        Args:
+            size: size - int
+
+        Returns:
+            None
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """Returns the instance size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the instance size"""
+
+        self.__validator(value)
+        self.__size = value
