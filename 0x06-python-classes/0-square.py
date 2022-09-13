9d16245d00f65d4dcd18cd349ab003e9022347ea
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-13 22:27:01 +0100

    Classes: Task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-"""I swear y'all are bunch of crazy people,
-Yeah I said it (Crazy people)
+
 """
+I swear y'all are bunch of crazy people,
+Yeah I said it (Crazy people)
+
+"""
+
+
 class Square:
     """A class representing an empty square."""
     pass
