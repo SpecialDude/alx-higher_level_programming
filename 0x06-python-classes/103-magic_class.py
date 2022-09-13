825398821d6e67bc72a8fa8e88c86e43b61abed6
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-14 00:57:18 +0100

    Classes: Task 10

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""I swear y'all are bunch of crazy people"""
+
+import math
+
+
+class MagicClass:
+    def __init__(self, radius):
+        """Initialization of circle"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """Area of circle"""
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """Circumference of circle"""
+        return 2 * math.pi * self.__radius
