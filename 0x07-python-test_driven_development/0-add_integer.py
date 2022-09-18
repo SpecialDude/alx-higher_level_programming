aa97b22fb996ac705efc93fd91448a9384fcc43a
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-18 11:57:25 +0100

    TDD: Task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""
+Add two integers, Example
+
+>>> add_integer(10, 5)
+15
+"""
+
+
+def add_integer(a, b=98):
+    """Add two integers
+
+    Args:
+        a: integer
+        b: also integer
+
+    Returns:
+        sum of a and b
+
+    >>> add_integer(10, 5)
+    15
+    >>> add_integer(10, )
+    108
+    >>> add_integer(10, 19.45)
+    29
+    >>> add_integer(10, "19.45")
+    Traceback (most recent call last):
+        ...
+    TypeError: b must be an integer
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return (int(a) + int(b))
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
