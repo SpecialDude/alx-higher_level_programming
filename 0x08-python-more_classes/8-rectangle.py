9c9b87326c41eaeaca35418dbc24b6ef82186225
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-21 02:01:34 +0100

    More Class: Task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -101,6 +101,6 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
 
-        if rect_2.area() > rect_1:
+        if rect_2.area() > rect_1.area():
             return rect_2
         return rect_1
