28f887aefb6dbff51b4d7c5c19c08157a059ad7a
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-21 02:33:42 +0100

    More Class: Task 10

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+A program that solves the N queens problem
+"""
+
+import sys
+
+
+def main():
+    """Program entry point"""
+
+    args = sys.argv[1:]
+
+    if len(args) != 1:
+        print("Usage: nqueens N")
+        exit(1)
+
+    if not args[0].isdigit():
+        print("N must be a number")
+        exit(1)
+
+    n = int(args[0])
+
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    
