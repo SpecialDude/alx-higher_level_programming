7cc4bdb6a215123f49c02672df5bccf9c1358909
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-28 00:38:35 +0100

    File IO: Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,23 +8,16 @@ This is script dumps all arguments to a json file
 
 import sys
 
-save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__(
-        "6-load_from_json_file.py"
+        "6-load_from_json_file"
     ).load_from_json_file
 
 
-def main():
-    """Program entry point
-    """
-    args = sys.argv[1:]
-    filename = "add_item.json"
+filename = "add_item.json"
+args = sys.argv[1:]
 
-    e_args = load_from_json_file(filename)
-    args.extend(e_args)
+e_args = load_from_json_file(filename)
+args.extend(e_args)
 
-    save_to_json_file(args, filename)
-
-
-if __name__ == "__main__":
-    main()
+save_to_json_file(args, filename)
