be18ea0dc95084db412d777ea03cd5980d018d5f
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-27 23:59:01 +0100

    File IO: Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This module contains a single function to return
+the json representation of objects
+
+write_file()
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Json representation of objects
+
+    Args:
+        my_obj: A python object
+
+    Returns:
+        JSON string
+    """
+
+    return json.dumps(my_obj)
