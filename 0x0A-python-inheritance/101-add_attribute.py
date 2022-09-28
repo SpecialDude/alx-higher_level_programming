12b6ab037907fe35c0074db31da322c4dd613b3a
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-28 20:37:47 +0100

    Inheritance: Task 13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -9,8 +9,7 @@ to a class
 def add_attribute(instance, name, value):
     """Add attribute to instance of a class"""
 
-    if hasattr(instance, '__slots__'):
+    if not hasattr(instance, name) and not hasattr(instance, '__dict__'):
         raise TypeError("can't add new attribute")
 
-    # if hasattr(instance, '__dict__'):
     setattr(instance, name, value)
