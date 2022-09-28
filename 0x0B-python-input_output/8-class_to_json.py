211b4b5ffa4df494884d82087fb5b06f5f7bf44f
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-28 01:07:21 +0100

    File IO: Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+This module contains a single function to return
+a class's attributes
+
+class_to_json()
+"""
+
+
+def class_to_json(obj):
+    """Returns all attributes of an object as a dictionary
+
+    Args:
+        obj: A python object
+        filename: str
+
+    Returns:
+        None
+    """
+
+    return obj.__dict__
