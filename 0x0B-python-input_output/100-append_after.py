368fb9d24d97d79253905cfc684b57fe3ed8ea82
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-28 03:12:42 +0100

    Classes: Task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+Module contains a functiona to
+do some file manipulation
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Read file and append some lines after
+
+    Args:
+        filename: Name of file
+        search_string: str
+        new_string: tbr str
+
+    Returns:
+        None
+    """
+
+    lines = []
+
+    with open(filename, mode="r") as fd:
+
+        for line in fd:
+            line = line.rstrip('\n')
+            lines.append(line + '\n')
+            if line == search_string:
+                lines.append(new_string + '\n')
+    if lines:
+        with open(filename, mode="w") as fd:
+            fd.writelines(lines)
