8d10cb5bae26e98f7ebcdad1bf1e8fa945021910
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-28 13:58:59 +0100

    Inheritance: Task 2

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,14 +3,6 @@
 """
 This module defines MyList class extending
 the builtin list class
-
->>> li = MyList([3, 0, 8])
->>> li
-[3, 0, 8]
->>> li.print_sorted()
-[0, 3, 8]
->>> li
-[3, 0, 8]
 """
 
 
@@ -18,14 +10,6 @@ class MyList(list):
     """
     Extending a list class to
     print list elements sorted
-
-    >>> myl = MyList([1, 4, 2, 3, 5])
-    >>> myl
-    [1, 4, 2, 3, 5]
-    >>> myl.print_sorted()
-    [1, 2, 3, 4, 5]
-    >>> myl
-    [1, 4, 2, 3, 5]
     """
 
     def print_sorted(self):
