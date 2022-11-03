02f6cace542070c9932cbee7b08cea498b99c9b3
SpecialDude <warithadetayo.awa@gmail.com>
2022-11-03 17:23:20 +0100

    SQL: Task 17

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,3 @@
 -- File Imports
 
-SET autocommit=0;
-source temperatures.sql;
-COMMIT;
-
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY value;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
