731fe2139bf12f779c5eb5af9c8615cbf7cc9f40
SpecialDude <warithadetayo.awa@gmail.com>
2022-11-03 16:00:48 +0100

    SQL: Task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- List all records with constraints
 
-SELECT name, score FROM second_table WHERE name is NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name is NOT NULL ORDER BY score DESC;
