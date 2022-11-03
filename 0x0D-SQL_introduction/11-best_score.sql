fee35477c0fbc12be4a948234535a2a9eedda0a6
SpecialDude <warithadetayo.awa@gmail.com>
2022-11-03 14:34:39 +0100

    SQL: Task 11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- Selects records from a table with constraints
 
-SELECT score, name FROM second_table IF score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
