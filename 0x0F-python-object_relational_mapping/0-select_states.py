358580fa2112b99bad4c297daa22af4a4667845c
SpecialDude <warithadetayo.awa@gmail.com>
2022-12-04 17:31:19 -0800

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,28 +7,38 @@ import sys
 import MySQLdb
 
 
-if __name__ != "__main__":
-    sys.exit()
+def main():
+    """
+        The main program
+        Readsall rows from a database table
+    """
 
-args = sys.argv[1:]
+    args = sys.argv[1:]
 
-if len(args) == 3:
-    username, password, dbname = args
-else:
-    sys.exit()
+    if len(args) == 3:
+        username, password, dbname = args
+    else:
+        sys.exit()
 
-db = MySQLdb.connect(
-    host='localhost',
-    user=username,
-    passwd=password,
-    db=dbname,
-    port=3306
-)
+    db = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=dbname,
+        port=3306
+    )
 
-cur = db.cursor()
+    cur = db.cursor()
 
-cur.execute("SELECT id, name FROM states")
-rows = cur.fetchall()
+    cur.execute("SELECT id, name FROM states")
+    rows = cur.fetchall()
 
-for row in rows:
-    print(row)
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
