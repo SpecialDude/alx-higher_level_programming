3678bbef51e9ddaa991b4aa3f524f55243ddf68c
SpecialDude <warithadetayo.awa@gmail.com>
2022-12-04 18:09:43 -0800

    Task 2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,45 @@
+
+#!/usr/bin/python3
+
+"""This module lists all states from a  database"""
+
+
+import sys
+import MySQLdb
+
+
+def main():
+    """
+        The main program
+        Readsall rows from a database table
+    """
+
+    args = sys.argv[1:]
+
+    if len(args) == 3:
+        username, password, dbname = args
+    else:
+        sys.exit()
+
+    db = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=dbname,
+        port=3306
+    )
+
+    cur = db.cursor()
+
+    cur.execute("SELECT id, name FROM states WHERE name LIKE N%")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
