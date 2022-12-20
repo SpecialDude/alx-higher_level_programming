6266dd8d65ef0dfdd07fbef835f5000f72c874f6
SpecialDude <warithadetayo.awa@gmail.com>
2022-12-20 08:10:49 +0100

    Task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This sends a POST request to the specified url
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
