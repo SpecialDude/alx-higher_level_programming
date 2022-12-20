418e776e9fcbfc130ce36898d826de5ff8378df6
SpecialDude <warithadetayo.awa@gmail.com>
2022-12-20 11:00:11 +0100

    Task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes request to a url
-curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -sLX PUT -d "user_id=98" -H "origin:HolbertonSchool" 0.0.0.0:5000/catch_me
