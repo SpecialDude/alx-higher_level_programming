848816cbad6887f41aa38f49484af569ec61f6a9
SpecialDude <warithadetayo.awa@gmail.com>
2023-01-18 07:49:44 +0100

    Task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -10,20 +10,28 @@ const movieId = process.argv[2];
 
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url, async function (error, response, body) {
+async function printName (url) {
+  await request(url, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+    }
+  });
+}
+
+function loadURL (urlList) {
+  for (let i = 0; i < urlList.length; i++) {
+    printName(urlList[i]);
+  }
+}
+
+request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const characters = JSON.parse(body).characters;
 
-    for (let i = 0; i < characters.length; i++) {
-      await request(characters[i], function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+    loadURL(characters);
   }
 });
