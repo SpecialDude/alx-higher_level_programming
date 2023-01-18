284003a1574c099fd2a8772e1c98248ccacfa9fe
SpecialDude <warithadetayo.awa@gmail.com>
2023-01-18 05:25:47 +0100

    Task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+// This Program opens a file and write into it
+
+const fs = require('fs');
+const request = require('request');
+
+if (process.argv.length < 4) { process.exit(1); }
+
+const url = process.argv[2];
+const filename = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  } else {
+    fs.writeFile(filename, body, 'utf-8', function (err) {
+      if (err) { console.log(err); }
+    });
+  }
+});
