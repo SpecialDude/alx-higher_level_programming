690a919c65cde14a980a59fa296ef940e1535340
SpecialDude <warithadetayo.awa@gmail.com>
2023-02-17 13:52:16 +0100

    Task_102

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
-  incr () {
-    this.value++;
-  }
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
 };
 
 myObject.incr();
