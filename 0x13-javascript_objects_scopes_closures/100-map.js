61a7b3f04ddeb03d256cbaa4490eed86698d52df
SpecialDude <warithadetayo.awa@gmail.com>
2023-02-20 18:27:59 +0100

    still_on_mapping_ooo

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,5 +3,5 @@
 const array = require('./100-data').list;
 
 console.log(array);
-const new_array = array.map((value, index, array) => value * index);
-console.log(new_array);
+const newArray = array.map((value, index, array) => value * index);
+console.log(newArray);
